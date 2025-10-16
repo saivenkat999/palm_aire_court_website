f3345b7833f4c2717eecdb83a39f1be5b8d022b3
Saivenkat Chepuri <saivenkatchepuri@gmail.com>
2025-10-16 22:35:54 +0530

    fix: use dynamic import for Vite to avoid loading in production

--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,6 @@
 import express, { type Request, Response, NextFunction } from "express";
 import { registerRoutes } from "./routes";
-import { setupVite, serveStatic, log } from "./vite";
+import { serveStatic, log } from "./vite";
 import path from "path";
 import { fileURLToPath } from 'url';
 
@@ -60,6 +60,7 @@ app.use((req, res, next) => {
   // setting up all the other routes so the catch-all route
   // doesn't interfere with the other routes
   if (app.get("env") === "development") {
+    const { setupVite } = await import("./vite");
     await setupVite(app, server);
   } else {
     serveStatic(app);
